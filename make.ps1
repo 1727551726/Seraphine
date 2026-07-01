92798e5564dc07f80daf86e341381860cc6da01f
Options <1727551726@qq.com>
2026-07-01 22:01:15 +0800

    构建脚本修改

--- make.ps1
+++ make.ps1
@@ -6,11 +6,13 @@
     输出的目标路径。默认为当前目录。
 
 .PARAMETER dbg
-    是否启用调试模式。如果启用，将不会删除 `.\dist` 目录，也不会创建 7z 文件。
+    是否启用调试模式。如果启用，将不会删除 `.\dist` 目录，也不会创建 ZIP 文件。
 
 .EXAMPLE
     .\make.ps1 -dbg
 #>
+
+# 输出格式：ZIP（使用 PowerShell 内置 Compress-Archive，无需安装 7-Zip）
 
 param(
     [Parameter()]
@@ -18,15 +20,19 @@ param(
     [Switch]$dbg
 )
 
-if ($dbg -and (Test-Path .\dist)) {
+$ErrorActionPreference = "Stop"
+
+if (Test-Path .\dist) {
     rm -r -Force .\dist
 }
 
-pyinstaller -w -i .\app\resource\images\logo.ico main.py
+pyinstaller -w -i .\app\resource\images\logo.ico --hidden-import backports.zstd --collect-all pyzstd --collect-all backports.zstd main.py
+Start-Sleep -Seconds 2
 rm -r -fo .\build
 rm -r -fo .\main.spec
-rni -path .\dist\main -newName Seraphine
-rni -path .\dist\Seraphine\main.exe -newName Seraphine.exe
+Copy-Item -Path ".\dist\main" -Destination ".\dist\Seraphine" -Recurse -Force
+Remove-Item -Path ".\dist\main" -Recurse -Force
+Rename-Item -Path ".\dist\Seraphine\main.exe" -NewName "Seraphine.exe"
 cpi .\app -destination .\dist\Seraphine -recurse
 rm -r .\dist\Seraphine\app\common
 rm -r .\dist\Seraphine\app\components
@@ -44,6 +50,6 @@ $files = Get-ChildItem -Path ".\dist\Seraphine\*" -Recurse |
 $files | Out-File -FilePath ".\dist\Seraphine\filelist.txt" -Encoding UTF8
 
 if (! $dbg) {
-    7z a $dest\Seraphine.7z .\dist\Seraphine\* -r
+    Compress-Archive -Path ".\dist\Seraphine\*" -DestinationPath "$dest\Seraphine.zip" -Force
     rm -r .\dist
 }
